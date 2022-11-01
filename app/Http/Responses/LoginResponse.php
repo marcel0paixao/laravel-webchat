77a3732deb993c68c4ed8588988e6816132aba60
marcel0paixao <celomar01@hotmail.com>
2022-11-01 10:04:46 -0300

    fixed register response

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -13,20 +13,7 @@ class LoginResponse implements LoginResponseContract
         $user = Auth::user();
         $route = 'internal-home';
 
-        if ($user->role_id > 1) {
-            if (session()->exists('initial_action')) {
-                $action = session()->get('initial_action');
-
-                //TODO: create constants file
-                if($action === 'post_job') {
-                    $route = 'internal-home';
-                } else if ($action === 'search_professional') {
-                    $route = 'internal-home';
-                }
-            }
-
-            session()->remove('initial_action');
-        }
+        //
 
         return $request->wantsJson()
             ? response()->json(['two_factor' => false])
