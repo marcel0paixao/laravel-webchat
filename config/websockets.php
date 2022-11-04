9004acc5b97263d24c5988f7a5ba2181804f94c5
marcel0paixao <celomar01@hotmail.com>
2022-11-04 17:02:54 -0300

    configuration of websockets

--- config/websockets.php
+++ config/websockets.php
@@ -29,7 +29,7 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'path' => env('PUSHER_APP_PATH'),
             'capacity' => null,
-            'enable_client_messages' => false,
+            'enable_client_messages' => false, // p2p connection
             'enable_statistics' => true,
         ],
     ],
